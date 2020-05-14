dbb41a215af08adf1841481e58c8e00fd3c8997b
Marie Belile <belilemarie@gmail.com>
2020-05-14 18:45:34 +0200

    rails mister cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# COCKTAILS
+puts "Cleaning database..."
+Cocktail.destroy_all
+
+puts "Creating cocktails..."
+martini = { name: "Martini" }
+pina_collada = { name: "Pina collada" }
+negroni = { name: "Negroni" }
+paloma = { name: "Paloma" }
+
+[ martini, pina_collada, negroni, paloma ].each do |attributes|
+  cocktail = Cocktail.create!(attributes)
+  puts "Created #{cocktail.name}"
+end
+puts "Finished!"
+
+# INGREDIENTS
+
+puts "Cleaning database..."
+Ingredient.destroy_all
+
+puts "Creating ingredients..."
+
+lemon = { name: "lemon" }
+ice = { name: "ice" }
+mint = { name: "mint leaves" }
+
+[ lemon, ice, mint ].each do |attributes|
+  ingredient = Ingredient.create!(attributes)
+  puts "Created #{ingredient.name}"
+end
+puts "Finished!"
+
+# DOSES
